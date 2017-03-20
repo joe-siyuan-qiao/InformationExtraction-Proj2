5ab60abaee9a30a2365f7d5577fa461787edacd1
Siyuan Qiao <joe.siyuan.qiao@gmail.com>
2017-03-20 14:35:41 -0400

    Pick fenonic baseforms done

--- python/hmm.py
+++ python/hmm.py
@@ -2,6 +2,8 @@
 The implementation of the fenonic baseforms for isolated word recognition
 """
 
+from copy import deepcopy
+
 
 class Fenon:
     """
@@ -19,11 +21,11 @@ class Fenon:
         the emission probabilities for the outputs
     """
 
-    def __init__(self, name, num_outputs):
+    def __init__(self, name):
         self.name = name
         self.id = Fenon.cvtname2id(name)
         self.trans = [0.8, 0.1, 0.1]
-        self.emiss = [[0.5 / 255] * num_outputs] * 3
+        self.emiss = [[0.5 / 255] * 256] * 3
         for i in range(3):
             self.emiss[i][self.id] = 0.5
 
@@ -31,3 +33,93 @@ class Fenon:
     def cvtname2id(name):
         s, e = name[0:2]
         return (ord(s) - 65) * 26 + ord(e) - 65
+
+
+class Silence:
+    """
+    The class of silence
+
+    Members:
+    -------
+    name: string
+        sil
+    id: int
+        256
+    trans: [] of size 12
+        the transition probabilities: t1, t2, ..., t12
+    emiss: [[] of size num_outputs specified in __init__] of size 12
+        the emission probabilities for the outputs
+    """
+
+    def __init__(self):
+        self.name = 'sil'
+        self.id = 256
+        self.trans = [1. / 12] * 12
+        self.emiss = [[1. / 256] * 256] * 12
+
+
+class Baseform:
+    """
+    The class of fenonic baseforms
+
+    Members:
+    -------
+    name: string
+        the name of the word
+    model: [] of Fenon/Silence
+        the fenonic baseform of the word name
+    """
+
+    def __init__(self, name):
+        self.name = name
+
+    def build(self, fenones, modelpool):
+        self.model = [deepcopy(modelpool[256]), ]
+        for fenon in fenones:
+            self.model.append(modelpool[Fenon.cvtname2id(fenon)])
+        self.model.append(deepcopy(modelpool[256]))
+
+
+class Trainer:
+    """
+    The trainer of fenonic baseforms
+
+    Members:
+    -------
+    modelpool: [] of fenones and silence
+        fenones and silence
+    trndata: [] of training data
+        the training data
+    devdata: [] of test data
+        the test data
+    """
+
+    def __init__(self):
+        pass
+
+    def read_fenones(self, filename):
+        self.modelpool = []
+        fin = open(filename, 'r')
+        lines = fin.readlines()
+        for i in range(1, len(lines)):
+            name = lines[i][0:2]
+            self.modelpool.append(Fenon(name))
+        self.modelpool.append(Silence())
+        fin.close()
+
+    def build_fenonic_baseforms(self, scr, pts, lbl):
+        scr, pts, lbl = open(scr, 'r'), open(pts, 'r'), open(lbl, 'r')
+        scrlines, ptslines, lbllines = scr.readlines()[1:], pts.readlines()[
+            1:], lbl.readlines()[1:]
+        self.fenonic_baseforms_fenones = {}
+        for i in range(len(scrlines)):
+            srcline = scrlines[i][:-1]
+            if srcline in self.fenonic_baseforms_fenones:
+                continue
+            s, e = ptslines[i].split(' ')[0:2]
+            s, e = int(s), int(e) - 1
+            lblline = lbllines[i][:-1].split(' ')[s:e]
+            self.fenonic_baseforms_fenones[srcline] = lblline
+        scr.close()
+        pts.close()
+        lbl.close()
--- test.py
+++ test.py
@@ -1,6 +1,15 @@
-import os, sys
+import os
+import sys
 sys.path.insert(0, 'python')
 import hmm
 
 id1 = hmm.Fenon.cvtname2id('AA')
 id2 = hmm.Fenon.cvtname2id('JV')
+lbl = os.path.join('data', 'clsp.lblnames')
+scr = os.path.join('data', 'clsp.trnscr')
+pts = os.path.join('data', 'clsp.endpts')
+lbl = os.path.join('data', 'clsp.trnlbls')
+
+trainer = hmm.Trainer()
+trainer.read_fenones(lbl)
+trainer.build_fenonic_baseforms(scr, pts, lbl)
