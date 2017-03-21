90407c1a81877980672981c32aa6b6e5a42d34df
Siyuan Qiao <joe.siyuan.qiao@gmail.com>
2017-03-20 22:01:24 -0400

    add timer

--- test.py
+++ test.py
@@ -1,7 +1,20 @@
 import os
 import sys
+import time
 sys.path.insert(0, 'python')
 import hmm
+
+class Timer(object):
+    def __init__(self, name=None):
+        self.name = name
+
+    def __enter__(self):
+        self.tstart = time.time()
+
+    def __exit__(self, type, value, traceback):
+        if self.name:
+            print '[%s]' % self.name,
+        print 'Elapsed: %s' % (time.time() - self.tstart)
 
 id1 = hmm.Fenon.cvtname2id('AA')
 id2 = hmm.Fenon.cvtname2id('JV')
@@ -15,6 +28,9 @@ trainer.read_fenones(lbl)
 trainer.pick_fenonic_baseforms(scr, pts, lbl)
 trainer.read_training_data(scr, lbl)
 # print trainer.training_data
-trainer.build_baseforms()
-trainer.init_training_trellis()
-trainer.forward()
+with Timer('build_baseforms'):
+    trainer.build_baseforms()
+with Timer('init_training_trellis'):
+    trainer.init_training_trellis()
+with Timer('forward'):
+    trainer.forward()
